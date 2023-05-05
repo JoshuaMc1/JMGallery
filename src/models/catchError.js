927e8f60f12d1cc954c74fdc4963fe74964b41e9
Joshua Mclean <86990609+JoshuaMc1@users.noreply.github.com>
2023-05-05 01:01:54 -0600

    Update catchError.js

--- src/models/catchError.js
+++ src/models/catchError.js
@@ -13,12 +13,6 @@
 export default function catchError(error) {
     const { response } = error;
 
-    console.log({
-        success: false,
-        status: response?.status || null,
-        statusText: response?.statusText || null,
-    });
-
     return {
         success: false,
         status: response?.status || null,
